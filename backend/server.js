dc42288aec772314332c7431f1fbff5e35a25b93
Najisha <najisha200116@gmail.com>
2026-08-14 11:17:43 +0530

    Prepare backend for deployment

--- backend/server.js
+++ backend/server.js
@@ -27,6 +27,11 @@ const PORT = process.env.PORT  || 3000
 
 app.use(cors());
 app.use(express.json());
+app.get("/", (req, res) => {
+  res.json({
+    message: "CHRONOS Backend is running"
+  });
+});
 
 
 app.use("/apiauth",authRoutes)
